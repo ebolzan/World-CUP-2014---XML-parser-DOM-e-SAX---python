bd7977a4120b4b03421a1d498a93f802b12ffc01
evandro <ebolzan@inf.ufsm.br>
2023-02-14 15:34:15 -0300

    feat(frontend): create js to list information2

--- frontend/js/Games.js
+++ frontend/js/Games.js
@@ -1,25 +1,42 @@
+class DateTimeGame{
+
+    constructor(datetimeInfo){
+        let textArray = datetimeInfo.trim().split(" ");
+
+        this.day= textArray[1];
+        this.monthAndYear = `${textArray[2]} de ${textArray[4]}`;
+        this.hour = textArray[5];
+
+    }
+}
+
 
 class Game{
     constructor(phaseName, datetimeInfo, teamA, teamB)
-    {
-        this.phaseName = phaseName;
-        this.datetimeInfo = datetimeInfo;
-        this.teamA = teamA;
-        this.teamB = teamB;
-       
-    }
+    {        
+        this.phaseName = phaseName.split(":")[1];
+    
+        this.datetimeInfo = new DateTimeGame(datetimeInfo);
 
+        this.teamA = teamA.split(":")[1];
+       
+        this.teamB = teamB.split(":")[1];       
+    }
 }
+
+
 
 const games_of= [];
 
 for(gameOne of games){
-    console.log(gameOne);
+  //  console.log(gameOne);
     let [a, b, c, d] = gameOne.split(",",4);
     games_of.push(new Game(a, b, c, d));
 }
 
+console.log(games_of);
+
 for(obj of games_of){
 
-    console.log(obj);
+    console.log(obj.datetimeInfo.monthAndYear);
 }
